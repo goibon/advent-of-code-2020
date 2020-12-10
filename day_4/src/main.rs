88d4ebc7e95a48b561e8e8d3d7d538868e6dd15f
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2020-12-10 23:44:18 +0100

    Rewrote solution to day 4 to solve part 2 of the problem.

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -29,6 +29,33 @@ fn count_valid_passports(passports: &Vec<&str>) -> u8 {
     count
 }
 
+fn count_valid_passports_part2(passports: &Vec<&str>) -> u8 {
+    let field_expressions: Vec<Regex> = vec![
+        Regex::new(r"byr:(19[2-9][0-9]|200[0-2])").unwrap(),
+        Regex::new(r"iyr:(201[0-9]|2020)").unwrap(),
+        Regex::new(r"eyr:(202[0-9]|2030)").unwrap(),
+        Regex::new(r"hgt:(((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in))").unwrap(),
+        Regex::new(r"hcl:#[0-9a-fA-F]{6}").unwrap(),
+        Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)").unwrap(),
+        Regex::new(r"pid:\d{9}(\D|$)").unwrap(),
+        ];
+
+    let mut valid_passport_count: u8 = 0;
+    for line in passports {
+        let mut valid_field_count = 0;
+        for expression in &field_expressions {
+            if expression.is_match(line) {
+                valid_field_count += 1;
+            }
+        }
+
+        if valid_field_count == field_expressions.len() {
+            valid_passport_count += 1;
+        }
+    }
+    valid_passport_count
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
@@ -38,5 +65,8 @@ fn main() {
     let input = split_input(&input);
 
     let count = count_valid_passports(&input);
-    println!("{:?}", count);
+    println!("part1: {:?}", count);
+
+    let count = count_valid_passports_part2(&input);
+    println!("part2: {:?}", count);
 }
